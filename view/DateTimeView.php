1e55991c10f320ba7357949f7ee1f7e507c9d33e
Jonas Strandqvist <js224nk@student.lnu.se>
2018-09-10 14:55:46 -0400

    corrected time with test case

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -5,7 +5,16 @@ class DateTimeView {
 
 	public function show() {
 
-		$date = (new DateTime())->format('Y-m-d H:i:s');
-		return "<p>" . $date. "</p>";
+		$dateObject = new DateTime();
+
+		$dayOfWeek = $dateObject->format('l');
+		$dayOfMonth = $dateObject->format('d');
+		$month = $dateObject->format('F');
+		$year = $dateObject->format('Y');
+		$time = $dateObject->format('H:i:s');
+
+		$dateString = $dayOfWeek . ", the " . $dayOfMonth . "th of ". $month . " " . $year . ", The time is " . $time;
+
+		return "<p>" . $dateString. "</p>";
 	}
 }
