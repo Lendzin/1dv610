8f25c658b16484c2c91a6d78e252679cbdfec22f
Jonas Strandqvist <js224nk@student.lnu.se>
2018-09-20 19:30:31 -0400

    added st/nd formating

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -7,14 +7,13 @@ class DateTimeView {
 	public function show() {
 
 		$dateObject = new \DateTime();
-		\date_modify($dateObject, "1 hours");
 		$dayOfWeek = $dateObject->format('l');
-		$dayOfMonth = $dateObject->format('d');
+		$dayOfMonth = $dateObject->format('dS');
 		$month = $dateObject->format('F');
 		$year = $dateObject->format('Y');
 		$time = $dateObject->format('H:i:s');
 
-		$dateString = $dayOfWeek . ", the " . $dayOfMonth . "th of ". $month . " " . $year . ", The time is " . $time;
+		$dateString = $dayOfWeek . ", the " . $dayOfMonth . " of ". $month . " " . $year . ", The time is " . $time;
 
 		return "<p>" . $dateString. "</p>";
 	}
