f4c5028d7fb2b78e2979362b2ac2d76e85e49ef0
Jonas Strandqvist <js224nk@student.lnu.se>
2018-10-12 18:45:57 -0400

    trying to fix online-issue with logout in test

--- view/LoginView.php
+++ view/LoginView.php
@@ -43,11 +43,11 @@ class LoginView {
 	private function generateLogoutButtonHTML() {
 		return '
 			<form  class="form" method="post" >
-				<fieldset class="fieldset">
+
 					<legend>Logged in as: "' .  $this->session->getSessionUsername() . '"</legend>
 					<p id="' . self::$messageId . '" class="' . $this->session->getSessionMessageClass() .'">' . $this->session->getSessionUserMessage() .'</p>
-					<input type="submit" class="button" name="' . self::$logout . '" value="logout"/>
-				</fieldset>
+					<input type="submit" name="' . self::$logout . '" value="logout"/>
+
 			</form>
 		';
 	}
